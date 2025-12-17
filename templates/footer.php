9510528ce0f7eaaf47f87f90d958416f497cd6d4
Guilherme <guilhermerochas0420@gmail.com>
2025-12-16 22:18:01 -0300

    chore: added header and footer

---
+++ templates/footer.php
@@ -0,0 +1,10 @@
+</div>
+
+<footer id="footer">
+    <div id="inner-footer">
+        Criado por: <a href="https://github.com/coderrocha" target="_blank">CoderRocha</a>
+    </div>
+</footer>
+</body>
+
+</html>
