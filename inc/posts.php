db1cb21490aaecca77e9f8c6a8f56a2ce9a4eb54
Guilherme <guilhermerochas0420@gmail.com>
2025-12-18 19:30:09 -0300

    feat: adicionado classe DB

--- inc/posts.php
+++ inc/posts.php
@@ -3,15 +3,10 @@
 /**
  * Retorna todos os posts
  */
-function get_all_posts()
-{
+function get_all_posts() {
     global $app_db;
-    $result = mysqli_query($app_db, "SELECT * FROM posts");
-    if (! $result) {
-        die(mysqli_error($app_db));
-    }
-
-    return mysqli_fetch_all($result, MYSQLI_ASSOC);
+	$result = $app_db->query( "SELECT * FROM posts" );
+	return $app_db->fetch_all( $result );
 }
 
 /**
@@ -21,36 +16,35 @@ function get_all_posts()
  * @param $excerpt
  * @param $content
  */
-function insert_post($title, $excerpt, $content)
-{
+function insert_post( $title, $excerpt, $content ) {
     global $app_db;
 
-    $published_on = date('d-m-Y H:i:s');
+	$published_on = date( 'Y-m-d H:i:s' );
+
+	$title = $app_db->real_escape_string( $title );
+	$excerpt = $app_db->real_escape_string( $excerpt );
+	$content = $app_db->real_escape_string( $content );
 
     $query = "INSERT INTO posts
 	( title, excerpt, content, published_on )
 	VALUES ( '$title', '$excerpt', '$content', '$published_on' )";
 
-    $result = mysqli_query($app_db, $query);
-    if (! $result) {
-        die(mysqli_error($app_db));
-    }
+	$result = $app_db->query( $query );
 }
 
 /**
  * Busca e retorna um Post
  * Se não encontrar, retorna false
  */
-function get_post($post_id)
-{
+function get_post( $post_id ) {
     global $app_db;
-    $query = "SELECT * FROM posts WHERE id = " . $_GET['view'];
-    $result = mysqli_query($app_db, $query);
-    if (! $result) {
-        die(mysqli_error($app_db));
-    }
 
-    return mysqli_fetch_assoc($result);
+	$post_id = intval( $post_id );
+
+	$query = "SELECT * FROM posts WHERE id = " . $post_id;
+	$result = $app_db->query( $query );
+
+	return $app_db->fetch_assoc( $result );
 }
 
 /**
@@ -58,12 +52,8 @@ function get_post($post_id)
  *
  * @param $id
  */
-function delete_post($id)
-{
+function delete_post( $id ) {
     global $app_db;
 
-    $result = mysqli_query($app_db, "DELETE FROM posts WHERE id = $id");
-    if (! $result) {
-        die(mysqli_error($app_db));
-    }
+	$result = $app_db->query( "DELETE FROM posts WHERE id = $id" );
 }
--- init.php
+++ init.php
@@ -1,18 +1,22 @@
 <?php
 
-if (!file_exists('config.php')) {
-    die('ERRO: O arquivo config.php não existe!');
+if (!file_exists( __DIR__ . '/config.php')) {
+	die('ERRO: O arquivo config.php não existe!');
 }
+
+session_start();
 
 require 'config.php';
 
 setlocale(LC_TIME, SITE_LANG);
 date_default_timezone_set(SITE_TIMEZONE);
 
-$app_db = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
-if ($app_db === false) {
-    die("Erro ao conectar com o banco de dados");
-}
+require( 'inc/class-db.php' );
+require( 'inc/posts.php' );
+require( 'inc/helpers.php' );
 
-require('inc/posts.php');
-require('inc/helpers.php');
+$app_db = new DB(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT);
+
+if ( isset( $_GET['logout'] ) ) {
+	logout();
+}
