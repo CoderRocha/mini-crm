d53904663cbe73762600f11fa53d21278c3f44c6
Guilherme <guilhermerochas0420@gmail.com>
2025-12-23 19:28:54 -0300

    refact: refatorado login

--- login.php
+++ login.php
@@ -3,6 +3,7 @@
 <?php
 
 $error = false;
+$error_message = '';
 
 if (isset($_POST['submit-login'])) {
     if (! check_hash('login', $_POST['hash'])) {
@@ -11,6 +12,9 @@ if (isset($_POST['submit-login'])) {
 
     if (! login($_POST['username'], $_POST['password'])) {
         $error = true;
+        $error_message = "Usuário ou senha incorretos.";
+    } else {
+        redirect_to('index.php');
     }
 }
 
@@ -25,15 +29,15 @@ if (is_logged_in()) {
 <h2>Login</h2>
 
 <?php if ($error): ?>
-    <div class="error"><?php echo "Usuário ou senha incorretos." ?></div>
+    <div class="error"><?php echo htmlspecialchars($error_message, ENT_QUOTES); ?></div>
 <?php endif; ?>
 
 <form action="" method="post">
     <label for="username">Usuário</label>
-    <input type="text" name="username" id="username">
+    <input type="text" name="username" id="username" required>
 
     <label for="password">Senha</label>
-    <input type="password" name="password" id="password">
+    <input type="password" name="password" id="password" required>
 
     <input type="hidden" name="hash" value="<?php echo htmlspecialchars(generate_hash('login'), ENT_QUOTES); ?>">
 
