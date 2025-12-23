3f234204fc8d774c140db43e14d250cd41c60184
Guilherme <guilhermerochas0420@gmail.com>
2025-12-23 19:32:45 -0300

    refact: refatorado header adm

--- templates/header.php
+++ templates/header.php
@@ -1,24 +1,33 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Micro CMS</title>
+	<title>Indie Games Hub</title>
 	<link rel="stylesheet" href="<?php echo SITE_URL; ?>/assets/style.css"></head>
 <body>
 
 <nav id="site-navigation" role="navigation" class="row row-center">
 	<div class="column">
 		<h1>
-			<a href="index.php">Mini CRM</a>
+			<a href="index.php">Indie Games Hub</a>
 		</h1>
 		<ul class="main-menu column clearfix">
 			<li><a href="<?php echo SITE_URL; ?>/index.php">Blog</a></li>
-			<?php if ( is_logged_in() ): ?>
-				<li><a href="<?php echo SITE_URL; ?>?logout=true">Logout</a></li>
+			<?php if ( is_logged_in() ): 
+				$current_user = get_logged_in_user();
+			?>
+				<li><a href="<?php echo SITE_URL; ?>/profile.php">Meu Perfil</a></li>
+				<?php if (can_create_post($current_user)): ?>
+					<li><a href="<?php echo SITE_URL; ?>/new-post.php">Novo Post</a></li>
+				<?php endif; ?>
+				<?php if (can_register_users($current_user)): ?>
+					<li><a href="<?php echo SITE_URL; ?>/register.php">Cadastrar Usuário</a></li>
+				<?php endif; ?>
 				<li><a href="<?php echo SITE_URL; ?>/admin">Painel Adm</a></li>
+				<li><a href="<?php echo SITE_URL; ?>?logout=true">Logout</a></li>
 			<?php else: ?>
 				<li><a href="<?php echo SITE_URL; ?>/login.php">Login</a></li>
 			<?php endif; ?>
